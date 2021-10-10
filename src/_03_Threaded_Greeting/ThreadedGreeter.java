0ff7e4150406a507841ce6c78c36b6158c4fca12
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2021-10-10 11:26:27 -0700

    threads

--- src/_03_Threaded_Greeting/ThreadedGreeter.java
+++ src/_03_Threaded_Greeting/ThreadedGreeter.java
@@ -1,16 +1,21 @@
 package _03_Threaded_Greeting;
 
-public class ThreadedGreeter {
-	int x;
-	
+public class ThreadedGreeter implements Runnable {
+int x;
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println(" Hello from thread number: " + x);
+		if(x<=50) {
+			int y = x+1;
+			
+			Thread x = new Thread((Runnable) new ThreadedGreeter(y));
+		}
+	}
+
 	public ThreadedGreeter(int i) {
 		x=1;
 	}
-public void run() {
-	System.out.println("Hello from thread number" + x);
-	Thread t = new Thread((Runnable) new ThreadedGreeter(x+1));
-	if(x<=50) {
-		Thread i = new Thread();
-	}
-}
+
+
 }
